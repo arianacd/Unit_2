bde4383f5d020348d75d1b07f34bd183bda1d002
ariana <ariana.daney@ssfs.org>
2019-09-18 14:40:28 -0400

    did almost 3 problems

---
+++ sphere.py
@@ -0,0 +1,14 @@
+# Problem #1
+volume_of_sphere = 4 / 3 * 3.14 * 5 ** 3
+print("The volume of the sphere is", volume_of_sphere)
+
+# problem #2
+book_price = ((24.95 * 60) * .6) + 3 + (.75 * 59)
+print ("the price of the books are", book_price)
+
+# problem #3
+running_time = (((8.25 * 2) + 21.8 + (6 * 60 + 52)) / 60)
+print("you would eat breakfast at", running_time)
+
+# problem #4
+
