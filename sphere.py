98d66e6d105b534ec84e25821d9e06e13dac910b
ariana <ariana.daney@ssfs.org>
2019-09-20 08:45:52 -0400

    did 3 classwork problems

--- sphere.py
+++ sphere.py
@@ -7,8 +7,25 @@ book_price = ((24.95 * 60) * .6) + 3 + (.75 * 59)
 print ("the price of the books are", book_price)
 
 # problem #3
+# fail
 running_time = (((8.25 * 2) + 21.8 + (6 * 60 + 52)) / 60)
 print("you would eat breakfast at", running_time)
 
-# problem #4
+# problem 2
+length = 14
+width = 7
+area_of_a_rectangle = length * width
+print("this is the area of a rectangle", area_of_a_rectangle)
+
+# problem 3
+triangle_height = 8
+triangle_base = 3
+area_of_a_triangle = triangle_height * triangle_base * .5
+print("the area of a trinagle is", area_of_a_triangle)
+
+# problem 4
+compound_interest = 10000 * (1 + (0.08/12)) ** (12 * 10)
+print("the compound interest is", compound_interest)
+
+
 
