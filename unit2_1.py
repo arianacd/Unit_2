515bb3155fcbb62422602355d3899e0fe580f382
ariana <ariana.daney@ssfs.org>
2019-09-23 10:14:34 -0400

    started unit 2 project option 1

---
+++ unit2_1.py
@@ -0,0 +1,22 @@
+# by Ariana Daney
+# Last modified September 23, 2019
+# Unit 2 final project, option 1
+
+name = (input("What is your name?"))
+print("Hello", name, ", my name is Pablo")
+
+location = (input("Where are you from?"))
+print("I have never been to", location, "but i would love to visit.")
+
+favorite_number = int(input("what is your favorite number?"))
+my_number = (favorite_number / 2)
+print("your favorite number, ", favorite_number, "is double of my favorite number", my_number)
+
+dream_car = input("What is your favorite car?")
+print("A", dream_car, "sounds like an awesome car")
+
+car_cost = float(input("What does your dream car cost?"))
+print("Wow!", car_cost, "is a lot!")
+
+years = input("How many years would you take a loan to get your car?")
+print("Well", years, "is a long time, good luck!")
