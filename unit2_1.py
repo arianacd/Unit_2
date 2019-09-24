9b2ebeb0e1d08f1a7adea6efde3e284f4fa4f340
ariana <ariana.daney@ssfs.org>
2019-09-24 11:19:03 -0400

    finished unit 2 project option 1

--- unit2_1.py
+++ unit2_1.py
@@ -3,14 +3,14 @@
 # Unit 2 final project, option 1
 
 name = (input("What is your name?"))
-print("Hello", name, ", my name is Pablo")
+print("Hello", name, ", my name is Pablo, its nice to meet you!")
 
 location = (input("Where are you from?"))
-print("I have never been to", location, "but i would love to visit.")
+print("I have never been to", location, "but I would love to visit")
 
 favorite_number = int(input("what is your favorite number?"))
 my_number = (favorite_number / 2)
-print("your favorite number, ", favorite_number, "is double of my favorite number", my_number)
+print("your favorite number, ", favorite_number, "is double of my favorite number", my_number, "!")
 
 dream_car = input("What is your favorite car?")
 print("A", dream_car, "sounds like an awesome car")
@@ -18,5 +18,14 @@ print("A", dream_car, "sounds like an awesome car")
 car_cost = float(input("What does your dream car cost?"))
 print("Wow!", car_cost, "is a lot!")
 
-years = input("How many years would you take a loan to get your car?")
-print("Well", years, "is a long time, good luck!")
+years = int(input("How many years would you take a loan to get your car"))
+
+annual_rate = float(input("Whats the annual interest rate for your car?"))
+monthly_interest = (annual_rate / 100) / 12
+number_of_monthly_payments = years * 12
+monthly_payment = ((monthly_interest * car_cost) / (1 - (1 + monthly_interest) ** -number_of_monthly_payments))
+total_cost = monthly_payment * (years * 12)
+print("Your monthly payment for your", dream_car, "would be $", monthly_payment, "and your total cost would be $",
+      total_cost, "good luck!")
+print("I have to go now", name, ", but it was nice talking to you!")
+
